06effacc3209de3d12822212b9e19b8dd3262cb2
soulbird <zhaothree@gmail.com>
2022-06-08 14:40:35 +0800

    feat: support build for macos

--- install-wasmtime.sh
+++ install-wasmtime.sh
@@ -16,14 +16,20 @@
 set -euo pipefail -x
 arch=$(uname -m | tr '[:upper:]' '[:lower:]')
 
+OS_NAME=linux
+os_name=$(uname -s | tr '[:upper:]' '[:lower:]')
+if [[ "${os_name}" == "darwin" ]]; then
+    OS_NAME=macos
+fi
+
 ARCH=$arch
 VER=v0.35.1
-wget https://github.com/bytecodealliance/wasmtime/releases/download/${VER}/wasmtime-${VER}-${ARCH}-linux-c-api.tar.xz
-tar -xvf ./wasmtime-${VER}-${ARCH}-linux-c-api.tar.xz > /dev/null
+wget https://github.com/bytecodealliance/wasmtime/releases/download/${VER}/wasmtime-${VER}-${ARCH}-${OS_NAME}-c-api.tar.xz
+tar -xvf ./wasmtime-${VER}-${ARCH}-${OS_NAME}-c-api.tar.xz > /dev/null
 if [ -d wasmtime-c-api ]; then
     rm -rf wasmtime-c-api
 fi
-mv wasmtime-${VER}-${ARCH}-linux-c-api wasmtime-c-api
+mv wasmtime-${VER}-${ARCH}-${OS_NAME}-c-api wasmtime-c-api
 if echo "int main(void) {}" | gcc -o /dev/null -v -x c - &> /dev/stdout| grep collect | tr -s " " "\012" | grep musl; then
     # build from source code if the libc is musl
     git clone https://github.com/bytecodealliance/wasmtime -b ${VER} --depth 1 \
