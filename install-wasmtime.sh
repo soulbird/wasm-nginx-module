ad8cf742161de206bc7fd609438072f4dc0c5695
soulbird <zhaothree@gmail.com>
2022-09-01 10:09:04 +0800

    force compile rumtime

--- install-wasmtime.sh
+++ install-wasmtime.sh
@@ -32,13 +32,10 @@ if [ -d wasmtime-c-api ]; then
     rm -rf wasmtime-c-api
 fi
 mv wasmtime-${VER}-${ARCH}-${os}-c-api wasmtime-c-api
-if echo "int main(void) {}" | gcc -o /dev/null -v -x c - &> /dev/stdout| grep collect | tr -s " " "\012" | grep musl; then
-    # build from source code if the libc is musl
-    git clone https://github.com/bytecodealliance/wasmtime -b ${VER} --depth 1 \
+git clone https://github.com/bytecodealliance/wasmtime -b ${VER} --depth 1 \
         && cd wasmtime \
         && git submodule update --init \
         && RUSTFLAGS="-C target-feature=-crt-static" \
            cargo build --release --manifest-path crates/c-api/Cargo.toml \
         && mv target/release/libwasmtime.* ../wasmtime-c-api/lib \
         && cd ..
-fi
